d0c5e90aebb9f342b8dff77e9667ed65cb84f349
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-10-24 19:30:10 +0100

    Update constant.dart

--- lib/widgets/constant.dart
+++ lib/widgets/constant.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color.fromRGBO(255, 214, 0, 1);
 const kSecondayColor = Color(0xFF558B2F);
+const kBgColor = Color(0xFFFC200); 
+const kTextColor = Color(0xFF241424); 
