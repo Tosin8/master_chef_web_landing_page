01de03a69c37cef647cf516b43a50e2af78c5e7f
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-10-25 21:48:43 +0100

    Updating the components

--- lib/component/landing_page/body.dart
+++ lib/component/landing_page/body.dart
@@ -6,47 +6,55 @@ class BodyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Text(
-          'We make Delicious Food',
-          style: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
+    return Padding(
+      padding: const EdgeInsets.only(left: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 40),
+          const Text(
+            'We make Delicious Food',
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
           ),
-        ),
-        const SizedBox(height: 5),
-        const Text(
-            'For your healthy living, master chef brings spicy receipe takes goes gently in your stomach, \n  done by our professional chefs in our restaurants. '),
-        const Spacer(),
-        Row(
-          children: [
-            TextButton(
-                style: ButtonStyle(
-                  foregroundColor: const MaterialStatePropertyAll(Colors.white),
-                  backgroundColor: MaterialStateProperty.all(kSecondaryColor),
-                ),
-                onPressed: () {},
-                child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 12, horizontal: 10),
-                    child: Text('Book a Table'.toUpperCase()))),
-            const SizedBox(width: 30),
-            TextButton(
+          const SizedBox(height: 5),
+          const Text(
+              'For your healthy living, master chef brings spicy receipe takes goes gently in your stomach, \n  done by our professional chefs in our restaurants. '),
+          const Spacer(),
+          Row(
+            children: [
+              TextButton(
+                  style: ButtonStyle(
+                    foregroundColor:
+                        const MaterialStatePropertyAll(Colors.white),
+                    backgroundColor: MaterialStateProperty.all(kSecondaryColor),
+                  ),
+                  onPressed: () {},
+                  child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 12, horizontal: 10),
+                      child: Text('Book a Table'.toUpperCase()))),
+              const SizedBox(width: 30),
+              TextButton(
                 style: ButtonStyle(
                   foregroundColor: const MaterialStatePropertyAll(Colors.white),
                   backgroundColor: MaterialStateProperty.all(kPrimaryColor),
                 ),
                 onPressed: () {},
                 child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 12, horizontal: 10),
-                    child: Text('View Menu'.toUpperCase())))
-          ],
-        )
-      ],
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 12, horizontal: 10),
+                  child: Text('View Menu'.toUpperCase()),
+                ),
+              ),
+              const Spacer(),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
